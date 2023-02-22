354c03b5e0b12f8adac824b4465942dc03fdc3f8
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-02-22 08:02:33 +0100

    Fix perms on cron

--- web_deploy.sh
+++ web_deploy.sh
@@ -66,8 +66,10 @@ function init_server {
 
     echo "# Setting up cron"
     echo '43 6 * * * certbot renew --post-hook "systemctl reload nginx"' > /etc/cron.d/certbot
+    chmod +x /etc/cron.d/certbot
     echo '0 */12 * * * root PATH=$PATH:/usr/bin:/usr/local/bin /root/.local/bin/borgmatic --verbosity -1 --syslog-verbosity 1' > /etc/cron.d/borgmatic
-    
+    chmod +x /etc/cron.d/borgmatic
+
     systemctl restart redis-server.service > /dev/null 2>&1
 
     echo "# Installation de Docker-compose"
