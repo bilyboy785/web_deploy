2402a7565d232e6ebce47c544d87f8205cfaa248
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-02-22 14:23:16 +0100

    ff

--- web_deploy.sh
+++ web_deploy.sh
@@ -366,7 +366,9 @@ case $1 in
         LE_EMAIL=$(cat /root/.le_email)
         echo "PRIMARY_DOMAIN=${PRIMARY_DOMAIN}" > ${ENV_FILE}
         echo "SECONDARY_DOMAIN=${SECONDARY_DOMAIN}" >> ${ENV_FILE}
-        echo "ADDITIONALS_ALIASES='${ADDITIONALS_ALIASES}'" >> ${ENV_FILE}
+        if [[ ! -z ${ADDITIONALS_ALIASES} ]]; then
+            echo 'ADDITIONALS_ALIASES="'${ADDITIONALS_ALIASES}'"' >> ${ENV_FILE}
+        fi
         echo "HOME_PATH=${HOME_PATH}" >> ${ENV_FILE}
         echo "PAM_USER=${PAM_USER}" >> ${ENV_FILE}
         echo "PAM_PASSWORD=${PAM_PASSWORD}" >> ${ENV_FILE}
