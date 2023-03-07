11c781dca113818df99316adcd48ea4a79d7d8e4
Martin Bouillaud <contact@bouillaudmartin.fr>
2023-03-07 22:02:03 +0100

    ff

--- cron.sh
+++ cron.sh
@@ -103,56 +103,57 @@ case $1 in
         fi
         ;;
     convertwebpavif)
-    if [[ ! -z $2 ]]; then
-        WEBSITE=$2
-        DOMAIN=$(cat $WEBSITE | grep PRIMARY_DOMAIN | cut -d\= -f2)
-        HOME_PATH=$(cat $WEBSITE | grep HOME_PATH | cut -d\= -f2)
-        WEB_PATH="${HOME_PATH}/web"
-        UPLOADS_PATH="${WEB_PATH}/wp-content/uploads"
-        OWNER=$(stat -c "%U" ${WEB_PATH})
-        for IMG in $(find $UPLOADS_PATH -type f -name '*.jpg' -or -name '*.jpeg' -or -name '*.png' -or -name '*.JPG' -or -name '*.JPEG' -or -name '*.PNG')
-        do
-            IMG_FILENAME=$(basename ${IMG})
-            IMG_DIRNAME=$(dirname ${IMG})
-            NEW_NAME_WEBP="${IMG_DIRNAME}/${IMG_FILENAME}.webp"
-            NEW_NAME_AVIF="${IMG_DIRNAME}/${IMG_FILENAME}.avif"
-            if [[ ! -f ${NEW_NAME_WEBP} ]]; then
-                    echo "   - ${NEW_NAME_WEBP}"
-                    cwebp -quiet -q 75 ${IMG} -o ${NEW_NAME_WEBP} > /dev/null 2>&1
-            fi
-            if [[ ! -f ${NEW_NAME_AVIF} ]]; then
-                    echo "   - ${NEW_NAME_AVIF}"
-                    convert -quiet -quality 75 ${IMG} ${NEW_NAME_AVIF} > /dev/null 2>&1
-            fi
-        done
-        chown -R ${OWNER}:www-data ${UPLOADS_PATH}
-    else
-        for WEBSITE in $(ls /opt/websites/*.env)
-        do
+        if [[ ! -z $2 ]]; then
+            WEBSITE=$2
             DOMAIN=$(cat $WEBSITE | grep PRIMARY_DOMAIN | cut -d\= -f2)
             HOME_PATH=$(cat $WEBSITE | grep HOME_PATH | cut -d\= -f2)
             WEB_PATH="${HOME_PATH}/web"
             UPLOADS_PATH="${WEB_PATH}/wp-content/uploads"
             OWNER=$(stat -c "%U" ${WEB_PATH})
-            if [[ -f ${WEB_PATH}/wp-config.php ]]; then
-                for IMG in $(find $UPLOADS_PATH -type f -name '*.jpg' -or -name '*.jpeg' -or -name '*.png' -or -name '*.JPG' -or -name '*.JPEG' -or -name '*.PNG')
-                do
-                        IMG_FILENAME=$(basename ${IMG})
-                        IMG_DIRNAME=$(dirname ${IMG})
-                        NEW_NAME_WEBP="${IMG_DIRNAME}/${IMG_FILENAME}.webp"
-                        NEW_NAME_AVIF="${IMG_DIRNAME}/${IMG_FILENAME}.avif"
-                        if [[ ! -f ${NEW_NAME_WEBP} ]]; then
-                                echo "   - ${NEW_NAME_WEBP}"
-                                cwebp -quiet -q 75 ${IMG} -o ${NEW_NAME_WEBP} > /dev/null 2>&1
-                        fi
-                        if [[ ! -f ${NEW_NAME_AVIF} ]]; then
-                                echo "   - ${NEW_NAME_AVIF}"
-                                convert -quiet -quality 75 ${IMG} ${NEW_NAME_AVIF} > /dev/null 2>&1
-                        fi
-                done
-                chown -R ${OWNER}:www-data ${UPLOADS_PATH}
-            fi
-        done
+            for IMG in $(find $UPLOADS_PATH -type f -name '*.jpg' -or -name '*.jpeg' -or -name '*.png' -or -name '*.JPG' -or -name '*.JPEG' -or -name '*.PNG')
+            do
+                IMG_FILENAME=$(basename ${IMG})
+                IMG_DIRNAME=$(dirname ${IMG})
+                NEW_NAME_WEBP="${IMG_DIRNAME}/${IMG_FILENAME}.webp"
+                NEW_NAME_AVIF="${IMG_DIRNAME}/${IMG_FILENAME}.avif"
+                if [[ ! -f ${NEW_NAME_WEBP} ]]; then
+                        echo "   - ${NEW_NAME_WEBP}"
+                        cwebp -quiet -q 75 ${IMG} -o ${NEW_NAME_WEBP} > /dev/null 2>&1
+                fi
+                if [[ ! -f ${NEW_NAME_AVIF} ]]; then
+                        echo "   - ${NEW_NAME_AVIF}"
+                        convert -quiet -quality 75 ${IMG} ${NEW_NAME_AVIF} > /dev/null 2>&1
+                fi
+            done
+            chown -R ${OWNER}:www-data ${UPLOADS_PATH}
+        else
+            for WEBSITE in $(ls /opt/websites/*.env)
+            do
+                DOMAIN=$(cat $WEBSITE | grep PRIMARY_DOMAIN | cut -d\= -f2)
+                HOME_PATH=$(cat $WEBSITE | grep HOME_PATH | cut -d\= -f2)
+                WEB_PATH="${HOME_PATH}/web"
+                UPLOADS_PATH="${WEB_PATH}/wp-content/uploads"
+                OWNER=$(stat -c "%U" ${WEB_PATH})
+                if [[ -f ${WEB_PATH}/wp-config.php ]]; then
+                    for IMG in $(find $UPLOADS_PATH -type f -name '*.jpg' -or -name '*.jpeg' -or -name '*.png' -or -name '*.JPG' -or -name '*.JPEG' -or -name '*.PNG')
+                    do
+                            IMG_FILENAME=$(basename ${IMG})
+                            IMG_DIRNAME=$(dirname ${IMG})
+                            NEW_NAME_WEBP="${IMG_DIRNAME}/${IMG_FILENAME}.webp"
+                            NEW_NAME_AVIF="${IMG_DIRNAME}/${IMG_FILENAME}.avif"
+                            if [[ ! -f ${NEW_NAME_WEBP} ]]; then
+                                    echo "   - ${NEW_NAME_WEBP}"
+                                    cwebp -quiet -q 75 ${IMG} -o ${NEW_NAME_WEBP} > /dev/null 2>&1
+                            fi
+                            if [[ ! -f ${NEW_NAME_AVIF} ]]; then
+                                    echo "   - ${NEW_NAME_AVIF}"
+                                    convert -quiet -quality 75 ${IMG} ${NEW_NAME_AVIF} > /dev/null 2>&1
+                            fi
+                    done
+                    chown -R ${OWNER}:www-data ${UPLOADS_PATH}
+                fi
+            done
+        fi
         apprise -vv -t "[${SRVHOSTNAME^^}] - WEBPAVIF" -b "Webp & Avif Images conversion Success !" tgram://${TG_TOKEN}/${TG_CHADID}/
         ;;
     fixperms)
