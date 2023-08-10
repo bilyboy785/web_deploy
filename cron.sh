9582f3bedaa57caf3bef702669af84637dee87c8
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-08-10 18:36:11 +0200

    ff

--- cron.sh
+++ cron.sh
@@ -307,7 +307,9 @@ case $1 in
             site=$2
             SITE_NAME=$(echo $site | sed 's/www\.//g')
             FTP_DOMAIN=$(echo $site | sed 's/www\.//g' | sed 's/demo1\.//g' | sed 's/demo2\.//g' | sed 's/demo3\.//g' | sed 's/dev\.//g')
-            NEW_CHECK=$(curl -s -X POST -d "url=https://${site}" -d "period=600" -d "alias=${FTP_DOMAIN}" -d "http_verbe='GET/HEAD'" -d "recipients[]=telegram:1830694333" https://updown.io/api/checks?api-key=Vy4Dw9BD35jU7eFMzWwg)
+            NEW_CHECK=$(curl -s -X POST -d "url=https://${site}" -d "period=600" -d "alias=${FTP_DOMAIN}" -d "http_verbe='GET/HEAD'" -d "recipients[]=telegram:1830694333" -d "apdex_t=1" -d \
+                        "disabled_locations[]=lan&disabled_locations[]=mia&disabled_locations[]=bhs&disabled_locations[]=sin&disabled_locations[]=tok&disabled_locations[]=syd" \
+                        https://updown.io/api/checks?api-key=Vy4Dw9BD35jU7eFMzWwg)
             echo $NEW_CHECK | jq '.'
         else
             for site in $(ls /var/www/html)
