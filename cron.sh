a6a8e6a0ea0477eb2d29706783e5928fd0841057
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-08-10 07:14:05 +0200

    fix

--- cron.sh
+++ cron.sh
@@ -286,7 +286,7 @@ case $1 in
                 echo "Site wordpress --> $site"
                 FULL_PATH="/var/www/html/$site/web"
                 OWNER=$(stat -c "%U" ${FULL_PATH})
-                echo "*/15 * * * * MAILTO=\"\" RID=\`uuidgen\` && curl -fsS -m 10 --retry 5 -o /dev/null ${HEALTHCHECK_UPDATE_URL}/start?rid=\$RID && /usr/local/bin/wp --path=${WEBROOT_PATH} cron event run --due-now && curl -fsS -m 10 --retry 5 -o /dev/null ${HEALTHCHECK_UPDATE_URL}?rid=\$RID" | crontab -u ${FULL_PATH} -
+                echo "*/15 * * * * MAILTO=\"\" RID=\`uuidgen\` && curl -fsS -m 10 --retry 5 -o /dev/null ${HEALTHCHECK_UPDATE_URL}/start?rid=\$RID && /usr/local/bin/wp --path=${WEBROOT_PATH} cron event run --due-now && curl -fsS -m 10 --retry 5 -o /dev/null ${HEALTHCHECK_UPDATE_URL}?rid=\$RID" | crontab -u ${OWNER} -
                 exit 0
             fi
         done
