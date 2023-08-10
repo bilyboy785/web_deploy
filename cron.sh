4ab09459c36e5242983912f9fabb8d19ff22d133
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-08-10 07:37:34 +0200

    Fix

--- cron.sh
+++ cron.sh
@@ -288,7 +288,7 @@ case $1 in
                 OWNER=$(stat -c "%U" ${FULL_PATH})
                 UPDATE_URL_HC=$(curl -s -X GET --header "X-Api-Key: PFyzt8uS_se--zYpr5KcJlendT-V5cek" "https://healthchecks.bldwebagency.fr/api/v3/checks/")
                 HC_PING_URL=$(echo $UPDATE_URL_HC | jq -r '.checks[] | select(.name | contains("'$site'"))' | jq -r '.ping_url')
-                echo -e "MAILTO=\"\"\n*/2 * * * *  RID=\`uuidgen\` && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL}/start?rid=\$RID && /usr/local/bin/wp --path=${FULL_PATH} cron event run --due-now && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL}?rid=\$RID" | crontab -u ${OWNER} -
+                echo -e "MAILTO=\"\"\n*/15 * * * *  RID=\`uuidgen\` && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL}/start?rid=\$RID && /usr/local/bin/wp --path=${FULL_PATH} cron event run --due-now && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL}?rid=\$RID" | crontab -u ${OWNER} -
                 exit 0
             fi
         done
