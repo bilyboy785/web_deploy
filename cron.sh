a76fe5dec3bd436e2fc49dc096fb1624f56b459f
Martin Bouillaud <contact@bouillaudmartin.fr>
2023-08-20 12:34:00 +0200

    ff

--- cron.sh
+++ cron.sh
@@ -306,8 +306,6 @@ case $1 in
                 HC_PING_URL_RESULT=$(echo $HC_PING_URL | jq -r '.ping_url')
                 echo -e "MAILTO=\"\"\n*/15 * * * *  RID=\`uuidgen\` && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL_RESULT}/start?rid=\$RID && /usr/local/bin/wp --path=${WEB_ROOT} cron event run --due-now && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL_RESULT}?rid=\$RID" | crontab -u ${OWNER} -
             else
-                echo "$SITE_NAME | Healthcheck already exists --> $HC_PING_URL"
-                echo " -> Updating cron"
                 echo -e "MAILTO=\"\"\n*/15 * * * *  RID=\`uuidgen\` && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL}/start?rid=\$RID && /usr/local/bin/wp --path=${WEB_ROOT} cron event run --due-now && curl -fsS -m 10 --retry 5 -o /dev/null ${HC_PING_URL}?rid=\$RID" | crontab -u ${OWNER} -
             fi
             if [[ -n $(find ${WEB_ROOT}/wp-content/plugins -type d -name "mailpoet") ]]; then
